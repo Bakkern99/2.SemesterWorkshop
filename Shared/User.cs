9cc7bc256d53b3cb5a7a9b62c93b166e7493b751
randahu <randa.hu01@gmail.com>
2025-04-07 10:17:55 +0200

    core

---
+++ Shared/User.cs
@@ -0,0 +1,13 @@
+namespace Shared;
+
+public class User
+{
+    public string Id { get; set; }
+    public string UserName { get; set; } = "";
+    public string Password { get; set; } = "";
+    public string Role { get; set; } = "admin";
+
+    public string mobilnummer { get; set; } = "";
+
+    public string Email { get; set; } = "";
+}
