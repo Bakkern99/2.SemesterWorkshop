7d315295ecdadfc4ee88344a835657cd4fea6e9a
randahu <randa.hu01@gmail.com>
2025-04-07 08:43:41 +0200

    test

--- 2.SemesterWorkshop/Core/Tøj.cs
+++ 2.SemesterWorkshop/Core/Tøj.cs
@@ -2,5 +2,15 @@ namespace 2.SemesterWorkshop.Core;
 
 public class Tøj
 {
+    public string Type { get; set; }
+    
+    public int størrelse { get; set; }
+    
+    public string farve { get; set; }
+    
+    public bool status { get; set; }
+    
+    public string EjerId { get; set; }
+    
     
 }
--- 2.SemesterWorkshop/Core/User.cs
+++ 2.SemesterWorkshop/Core/User.cs
@@ -2,5 +2,15 @@ namespace 2.SemesterWorkshop.Core;
 
 public class User
 {
+    public string Id { get; set; }
+    public string UserName { get; set; }
+    public string Password { get; set; }
+    public string Role { get; set; }
+    
+    public string mobilnummer { get; set; }
+    
+    public string Email { get; set; }
+    
+    
     
 }
